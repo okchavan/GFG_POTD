66ec4e3d5153c4b61b57a3871d57e282212df4f1
okchavan <omkarchavan1105@gmail.com>
2023-06-11 19:38:08 +0530

    commit

---
+++ AddingOnce.cpp
@@ -0,0 +1,16 @@
+class Solution{
+    public:
+    void update(int a[], int n, int updates[], int k)
+    {
+        // Your code goes here
+        for(int i=0;i<k;i++)
+        {
+            a[updates[i]-1]=a[updates[i]-1]+1;
+        }
+        
+        for(int i=1;i<n;i++)
+        {
+            a[i]=a[i-1]+a[i];
+        }
+    }
+};
