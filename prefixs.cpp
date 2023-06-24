f2f9f1933d64670e343888a7c6979146cb1117b2
okchavan <omkarchavan1105@gmail.com>
2023-06-24 22:09:12 +0530

    commit

---
+++ prefixs.cpp
@@ -0,0 +1,19 @@
+
+class Solution{   
+public:
+    int klengthpref(string arr[], int n, int k, string str){    
+        if (k > str.length())
+        return 0;
+    string temp = str.substr(0, k);
+
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (temp == arr[i].substr(0, k))
+        {
+            count++;
+        }
+    }
+    return count;
+    }
+};
